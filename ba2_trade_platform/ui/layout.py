69ac442cccf6285041e981d2611ca7d0ced7fe4d
Bastien Migette <bastien.migette@gmail.com>
2025-09-17 20:09:23 +0200

    refactor ui

--- ba2_trade_platform/ui/layout.py
+++ ba2_trade_platform/ui/layout.py
@@ -1,16 +1,25 @@
 from contextlib import contextmanager
 
-from menu import menu
+from .menu import menu
 
 from nicegui import ui
 
 
 @contextmanager
-def frame(navigation_title: str):
+def layout_render(navigation_title: str):
     """Custom page frame to share the same styling and behavior across all pages"""
-    ui.colors(primary='#6E93D6', secondary='#53B689', accent='#111B1E', positive='#53B689')
+    ui.colors(primary='#36454F', secondary='#53B689', accent='#111B1E', positive='#53B689')
+    with ui.footer(value=False) as footer:
+        ui.label('Footer')
+
+    with ui.left_drawer().classes('bg-[#36454F] text-white') as left_drawer:
+        ui.label('Side menu')
+
+    with ui.page_sticky(position='bottom-right', x_offset=20, y_offset=20):
+        ui.button(on_click=footer.toggle, icon='contact_support').props('fab')
+
     with ui.header():
-        ui.label('Modularization Example').classes('font-bold')
+        ui.button(on_click=lambda: left_drawer.toggle(), icon='menu').props('flat color=white')
         ui.space()
         ui.label(navigation_title)
         ui.space()
